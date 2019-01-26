31e8e4fa0cbd0fe34e73f10c6f9c1b374a0c0fa2
Chris Runnells <chris.runnells@automattic.com>
2019-01-25 15:49:50 -1000

    Revert "Update color filters to work in the editor"
    This reverts commit 541e90139222f4a1d8068ec748f610940d099e7a.

--- inc/wpcom.php
+++ inc/wpcom.php
@@ -6,7 +6,6 @@ function modern2019_primary_color_hue() {
     return 330;
 }
 add_filter( 'twentynineteen_default_hue', 'modern2019_primary_color_hue' );
-add_filter( 'twentynineteen_custom_colors_hue', 'modern2019_primary_color_hue' );
 
 // Our filter callback function
 function modern2019_primary_color_saturation() {
@@ -14,7 +13,6 @@ function modern2019_primary_color_saturation() {
     return 53;
 }
 add_filter( 'twentynineteen_default_saturation', 'modern2019_primary_color_saturation' );
-add_filter( 'twentynineteen_custom_colors_saturation', 'modern2019_primary_color_saturation' );
 
 // Our filter callback function
 function modern2019_primary_color_lightness() {
@@ -22,4 +20,3 @@ function modern2019_primary_color_lightness() {
     return 50;
 }
 add_filter( 'twentynineteen_default_lightness', 'modern2019_primary_color_lightness' );
-add_filter( 'twentynineteen_custom_colors_lightness', 'modern2019_primary_color_lightness' );
